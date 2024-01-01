a5028468d1a9769ce7c5c51e55eca9b67acd1b2f
Theoryofrelativity1915 <eliengelhart@gmail.com>
2024-01-01 13:01:24 -0500

    Logo asset added.

---
+++ SteelManufacturer/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <span className='text-xl font-bold text-red-400'>Hello Tailwind world!</span>
+    </div>
+  )
+}
+
+export default App
